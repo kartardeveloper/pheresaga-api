ea85c62913acdb7191ec08d703de5134f8854c31
Developer <kartardeveloper@gmail.com>
2025-02-11 17:06:02 +0530

    Updated

--- src/api/about/controller.js
+++ src/api/about/controller.js
@@ -1,44 +1,52 @@
-const About = require('../../../models/about');
+const About = require("../../../models/about");
 
 module.exports = {
-    createAbout:async (req, res)=>{
-        try {
-            const abouts = await About.create(req.body);
-            return res.status(200).json({data:abouts});
-        } catch (err) {
-            return res.status(400).json({error:err.messsage});
-        }
-    },
-    getAllAbouts: async(req, res)=> {
-        try {
-            const abouts = await About.find();
-            return res.status(200).json({data:abouts});
-        } catch (err) {
-            return res.status(400).json({error:err.messsage});
-        }
-    },
-    // getAboutById: async (req, res) => {
-    //     try {
-    //         const abouts = await About.findById(req.params.id);
-    //         return res.status(200).json({data:abouts});
-    //     } catch (err) {
-    //         return res.status(400).json({error:err.messsage});
-    //     }
-    // },
-    updateAbout :  async (req, res) =>{
-        try {
-            const abouts = await About.findByIdAndUpdate(req.query.id, req.body, { new: true });
-            return res.status(200).json({data:abouts});
-        } catch (err) {
-            return res.status(400).json({error:err.messsage});
-        }
-    },
-    deleteAbout: async (req, res)=> {
-        try {
-            await About.findByIdAndDelete(req.query.id);
-            return res.status(200).json({message:'About deleted'});                                                                                                                 
-        } catch (err) {
-            return res.status(400).json({error:err.messsage});
-        }
+  createAbout: async (req, res) => {
+    try {
+      const abouts = await About.create(req.body);
+      return res
+        .status(200)
+        .json({ data: abouts, message: "About created successfully" });
+    } catch (err) {
+      return res.status(400).json({ error: err.messsage });
     }
+  },
+  getAllAbouts: async (req, res) => {
+    try {
+      const abouts = await About.find();
+      return res
+        .status(200)
+        .json({ data: abouts, message: "About retrieved successfully" });
+    } catch (err) {
+      return res.status(400).json({ error: err.messsage });
+    }
+  },
+  // getAboutById: async (req, res) => {
+  //     try {
+  //         const abouts = await About.findById(req.params.id);
+  //         return res.status(200).json({data:abouts});
+  //     } catch (err) {
+  //         return res.status(400).json({error:err.messsage});
+  //     }
+  // },
+  updateAbout: async (req, res) => {
+    try {
+      const abouts = await About.findByIdAndUpdate(req.query.id, req.body, {
+        new: true,
+      });
+      return res
+        .status(200)
+        .json({ data: abouts, message: "About updated successfully" });
+    } catch (err) {
+      return res.status(400).json({ error: err.messsage });
+    }
+  },
+  deleteAbout: async (req, res) => {
+    try {
+      await About.findByIdAndDelete(req.query.id);
+      return res.status(200).json({ message: "About deleted successfully" });
+    } catch (err) {
+      return res.status(400).json({ error: err.messsage });
+    }
+  },
 };
